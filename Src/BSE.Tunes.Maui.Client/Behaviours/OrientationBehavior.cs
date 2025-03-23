bf3c0cb9aa0f98db1dabc464f499ef56ed4e34f0
Uwe Eichkorn <git.bsetunes@outlook.com>
2025-03-23 08:21:42 +0100

    cardsview removed

---
+++ Src/BSE.Tunes.Maui.Client/Behaviours/OrientationBehavior.cs
@@ -0,0 +1,41 @@
+﻿namespace BSE.Tunes.Maui.Client.Behaviours
+{
+    public class OrientationBehavior : Behavior<VisualElement>
+    {
+        protected override void OnAttachedTo(VisualElement bindable)
+        {
+            base.OnAttachedTo(bindable);
+
+            // Set initial state based on the current orientation
+            UpdateVisualState(DeviceDisplay.MainDisplayInfo.Orientation, bindable);
+
+            // Subscribe to orientation changes
+            DeviceDisplay.MainDisplayInfoChanged += (sender, e) =>
+            {
+                UpdateVisualState(e.DisplayInfo.Orientation, bindable);
+            };
+        }
+
+        protected override void OnDetachingFrom(VisualElement bindable)
+        {
+            base.OnDetachingFrom(bindable);
+
+            // Unsubscribe from orientation changes
+            DeviceDisplay.MainDisplayInfoChanged -= (sender, e) =>
+            {
+                UpdateVisualState(e.DisplayInfo.Orientation, bindable);
+            };
+        }
+
+        //private void OnMainDisplayInfoChanged(object sender, DisplayInfoChangedEventArgs e)
+        //{
+        //    UpdateVisualState(e.DisplayInfo.Orientation, (VisualElement)sender);
+        //}
+
+        private void UpdateVisualState(DisplayOrientation orientation, VisualElement element)
+        {
+            string state = orientation == DisplayOrientation.Portrait ? "Portrait" : "Landscape";
+            VisualStateManager.GoToState(element, state);
+        }
+    }
+}
